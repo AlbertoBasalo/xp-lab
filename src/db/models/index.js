4661bddddffc98cda667a5a95ab085a56e57a70d
Alberto Basalo <albertobasalo@hotmail.com>
2023-10-11 09:52:57 +0200

    clean: test and config

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -4,18 +4,15 @@ const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const process = require("process");
+
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "./../config/config.json")[env];
 const db = {};
 
 let sequelize = new Sequelize(config.url, config);
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
 
+/** read models from file system */
 fs.readdirSync(__dirname)
   .filter((file) => {
     return file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js" && file.indexOf(".test.js") === -1;
@@ -25,12 +22,14 @@ fs.readdirSync(__dirname)
     db[model.name] = model;
   });
 
+/** run associate function to create relations between models */
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 
+/** Export the client instance and the static helper */
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
--- tests/index.js
+++ tests/index.js
@@ -2,6 +2,27 @@ const axios = require("axios");
 const { USERS, ACTIVITIES } = require("./input.data");
 const { REGISTER_URL, USERS_URL, ACTIVITIES_URL, OPTIONS_BASE, LOGIN_URL, BOOKINGS_URL } = require("./api.settings");
 
+/**
+ * Main testing flows
+ */
+const main = async () => {
+  const alice = USERS[0];
+  const aliceActivities = ACTIVITIES[0];
+  const bob = USERS[1];
+  const bobActivities = ACTIVITIES[1];
+  try {
+    await registerUser(alice);
+    await registerUser(bob);
+    await organizeUserActivities(alice, aliceActivities);
+    await organizeUserActivities(bob, bobActivities);
+    await organizeAndBookActivity(alice, bob, aliceActivities[0]);
+  } catch (err) {
+    console.error(err.message, { path: err.request?.path, response: err.response?.data });
+  }
+};
+
+/** Test cases */
+
 const registerUser = async (user) => {
   const userToken = await getUserToken(user);
   const userOptions = makeUserOptions(userToken.accessToken);
@@ -19,31 +40,19 @@ const organizeUserActivities = async (organizer, activities) => {
 const organizeAndBookActivity = async (organizer, participant, activity) => {
   const { activityResult, organizerOptions, organizerToken } = await organizeActivity(organizer, activity);
   const { bookingResult, participantOptions, participantToken } = await bookActivity(participant, activityResult.data);
-  await clearOrganizeAndBook(
-    activityResult,
-    organizerOptions,
-    organizerToken,
-    bookingResult,
-    participantOptions,
-    participantToken
-  );
+  await clearActivityBookings(activityResult, organizerOptions, bookingResult, participantOptions);
+  await unregister(organizerToken.id, organizerOptions);
+  await unregister(participantToken.id, participantOptions);
 };
 
-async function clearOrganizeAndBook(
-  activityResult,
-  organizerOptions,
-  organizerToken,
-  bookingResult,
-  participantOptions,
-  participantToken
-) {
-  await deleteActivity(activityResult.data.id, organizerOptions);
-  await unregister(organizerToken.id, organizerOptions);
-  await deleteBooking(bookingResult.data.id, participantOptions);
-  await unregister(participantToken.id, participantOptions);
-}
+/** Helper functions */
 
-async function bookActivity(participant, activity) {
+const clearActivityBookings = async (activityResult, organizerOptions, bookingResult, participantOptions) => {
+  await deleteActivity(activityResult.data.id, organizerOptions);
+  await deleteBooking(bookingResult.data.id, participantOptions);
+};
+
+const bookActivity = async (participant, activity) => {
   const participantToken = await getUserToken(participant);
   const participantOptions = makeUserOptions(participantToken.accessToken);
   const booking = { activityId: activity.id, participants: 2, status: "confirmed" };
@@ -53,17 +62,17 @@ async function bookActivity(participant, activity) {
   const retrievedData = retrieved.data;
   console.log("Booking retrieved", retrievedData);
   return { bookingResult, participantOptions, participantToken };
-}
+};
 
-async function organizeActivity(organizer, activity) {
+const organizeActivity = async (organizer, activity) => {
   const organizerToken = await getUserToken(organizer);
   const organizerOptions = makeUserOptions(organizerToken.accessToken);
   const activityResult = await postActivity(activity, organizerOptions);
   console.log("Activity created", activityResult.data);
   return { activityResult, organizerOptions, organizerToken };
-}
+};
 
-async function getUserToken(user) {
+const getUserToken = async (user) => {
   try {
     const response = await register(user);
     console.log(`User ${user.email} registered`, response.data);
@@ -73,18 +82,20 @@ async function getUserToken(user) {
     console.log(`User ${user.email} logged in`, response.data);
     return response.data;
   }
-}
-async function organizeActivities(activities, userOptions) {
+};
+const organizeActivities = async (activities, userOptions) => {
   await Promise.all(activities.map((activity) => postActivity(activity, userOptions)));
   const retrieved = (await getMyActivities(userOptions)).data;
-  console.log("User data retrieved", retrieved);
+  console.log("Activities retrieved", retrieved);
   return retrieved;
-}
-async function clearUserJourney(activitiesResult, userOptions, userToken, user) {
+};
+const clearUserJourney = async (activitiesResult, userOptions, userToken, user) => {
   await Promise.all(activitiesResult.map((activity) => deleteActivity(activity.id, userOptions)));
   await unregister(userToken.id, userOptions);
   console.log("User journey cleared", user);
-}
+};
+
+/** API calls */
 
 const register = async (user) => await axios.post(REGISTER_URL, user, OPTIONS_BASE);
 const unregister = async (userId, options) => await axios.delete(`${USERS_URL}/${userId}`, options);
@@ -96,29 +107,10 @@ const getMyActivities = async (options) => await axios.get(`${ACTIVITIES_URL}/mi
 const postBooking = async (booking, options) => await axios.post(BOOKINGS_URL, booking, options);
 const deleteBooking = async (bookingId, options) => await axios.delete(`${BOOKINGS_URL}/${bookingId}`, options);
 const getMyBookings = async (options) => await axios.get(`${BOOKINGS_URL}`, options);
-
 const makeUserOptions = (token) => ({ headers: { ...OPTIONS_BASE.headers, Authorization: `Bearer ${token}` } });
-const expectedError = (err) => console.log(`Expected Error: ${err.response.status}, ${err.response.data.message}`);
-const expectedResult = (res) => console.log(`Expected Result: ${res.status}, ${res.data.length || 1} items`);
-const logError = (err) => console.log(`UN - Expected Error: ${err.message}`, err.request.path);
 
 /**
- * Main testing flows
- */
-main = async () => {
-  const alice = USERS[0];
-  const aliceActivities = ACTIVITIES[0];
-  const bob = USERS[1];
-  const bobActivities = ACTIVITIES[1];
-  try {
-    await registerUser(alice);
-    await registerUser(bob);
-    await organizeUserActivities(alice, aliceActivities);
-    await organizeUserActivities(bob, bobActivities);
-    await organizeAndBookActivity(alice, bob, aliceActivities[0]);
-  } catch (err) {
-    console.error(err.message, { path: err.request?.path, response: err.response?.data });
-  }
-};
-/** Start tests */
+ * Start tests
+ * Execute main function
+ **/
 main();
