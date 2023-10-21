636a9812650db86488a9d6eb7ad69e405639ed8b
Alberto Basalo <albertobasalo@hotmail.com>
2023-10-21 11:22:18 +0200

    feat: api index

--- src/api/_api.index.js
+++ src/api/_api.index.js
@@ -4,18 +4,26 @@ const credentialsRouter = require("./credentials.router");
 const usersRouter = require("./users.router");
 const middleware = require("../middleware/_middleware.index");
 const { guardUser } = middleware.security;
+const express = require("express");
 
-const configureRoutes = (app, apiVersion) => {
+const useRouters = (app, apiVersion) => {
   app.get("/", (req, res) => res.send("Activity Bookings API"));
-  app.use(`/v${apiVersion}/activities`, activitiesRouter);
-  app.use(`/v${apiVersion}/bookings`, bookingsRouter);
-  app.use(`/v${apiVersion}/credentials`, credentialsRouter);
-  app.use(`/v${apiVersion}/users`, guardUser, usersRouter);
+  const apiRouter = useApiRouters();
+  app.use(`/v${apiVersion}`, apiRouter);
+};
+
+const useApiRouters = () => {
+  const apiRouter = express.Router();
+  apiRouter.use("/activities", activitiesRouter);
+  apiRouter.use("/bookings", bookingsRouter);
+  apiRouter.use("/credentials", credentialsRouter);
+  apiRouter.use("/users", guardUser, usersRouter);
+  return apiRouter;
 };
 
 /**
  * Defines main routes for the API, attaching controllers.
  */
-const api = { configureRoutes };
+const api = { useRouters };
 
 module.exports = api;
--- src/app.js
+++ src/app.js
@@ -10,7 +10,9 @@ app.use(express.json());
 
 middleware.logs.useLoggers(app);
 middleware.security.useSecurity(app);
-api.configureRoutes(app, env.API_VERSION);
+
+api.useRouters(app, env.API_VERSION);
+
 middleware.errors.useErrorHandler(app, logger);
 
 const startMessage = `Listening on ${env.PORT}, ${env.NODE_ENV} environment`;
